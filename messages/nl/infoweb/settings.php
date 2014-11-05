65ee1b501af2e9de8741db43f19ffbc9511d1f12
Fabio Maggio <fabio@infoweb.be>
2014-11-05 15:31:59 +0100

    Create translation file

---
+++ messages/nl/infoweb/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Setting' => 'Instelling',
+    'Setting ID' => 'Instelling',
+    'Settings' => 'Instellingen',
+    'Translateable' => 'Meertalig',
+    'Value' => 'Waarde',
+];
