bd6bd4a467ca5995068dc03f972f9eb07b056780
Fabio Maggio <fabio@infoweb.be>
2014-11-06 09:15:06 +0100

    Create migration for the default settings

---
+++ migrations/m141106_074604_create_default_settings.php
@@ -0,0 +1,112 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+use infoweb\settings\models\Setting;
+use infoweb\settings\models\SettingValue;
+
+class m141106_074604_create_default_settings extends Migration
+{
+    public function up()
+    {
+        // Load application languages
+        $languages = Yii::$app->params['languages'];
+        
+        // Create SEO settings
+        $seoSettings = [
+            'seo/meta/title'                        => [
+                'label'         => 'Metatag - Title',
+                'value'         => '',
+                'translateable' => 1
+            ],
+            'seo/meta/description'                  => [
+                'label'         => 'Metatag - Description',
+                'value'         => '',
+                'translateable' => 1
+            ],
+            'seo/meta/keywords'                     => [
+                'label'         => 'Metatag - Keywords',
+                'value'         => '',
+                'translateable' => 1
+            ],
+            'seo/analytics/email-google-account'    => [
+                'label'         => 'E-mailadres Google account',
+                'value'         => '',
+                'translateable' => 0
+            ]               
+        ];
+        
+        foreach ($seoSettings as $k => $v) {
+            // Create setting
+            $setting = new Setting([
+                'key'           => $k,
+                'category_id'   => 2,
+                'label'         => $v['label'],
+                'type'          => Setting::TYPE_USER_DEFINED,
+                'template'      => Setting::TEMPLATE_TEXT,
+                'translateable' => $v['translateable']
+            ]);
+            
+            $setting->save(false);
+            
+            // Set values
+            foreach ($languages as $languageId => $languageName) {
+                $setting->language = $languageId;
+                $setting->value = $v['value'];
+                $setting->saveTranslation();    
+            }    
+        }
+
+        // Create Social settings
+        $socialSettings = [
+            'social/facebook'   => [
+                'label'         => 'Facebook account',
+                'value'         => 'https://www.facebook.com',
+                'translateable' => 1
+            ],
+            'social/twitter'    => [
+                'label'         => 'Twitter account',
+                'value'         => 'https://www.twitter.com/',
+                'translateable' => 1
+            ],
+            'social/google+'    => [
+                'label'         => 'Google+ account',
+                'value'         => 'https://plus.google.com',
+                'translateable' => 1
+            ],
+            'social/linkedin'   => [
+                'label'         => 'LinkedIn account',
+                'value'         => 'https://www.linkedin.com/',
+                'translateable' => 1
+            ]    
+        ];
+        
+        foreach ($socialSettings as $k => $v) {
+            // Create setting
+            $setting = new Setting([
+                'key'           => $k,
+                'category_id'   => 3,
+                'label'         => $v['label'],
+                'type'          => Setting::TYPE_USER_DEFINED,
+                'template'      => Setting::TEMPLATE_TEXT,
+                'translateable' => $v['translateable']
+            ]);
+            
+            $setting->save(false);
+            
+            // Set values
+            foreach ($languages as $languageId => $languageName) {
+                $setting->language = $languageId;
+                $setting->value = $v['value'];
+                $setting->saveTranslation();    
+            }    
+        }
+    }
+
+    public function down()
+    {
+        echo "m141106_074604_create_default_settings cannot be reverted.\n";
+
+        return false;
+    }
+}
--- models/Setting.php
+++ models/Setting.php
@@ -14,6 +14,7 @@ class Setting extends \yii\db\ActiveRecord
 {
     const TYPE_SYSTEM = 'system';
     const TYPE_USER_DEFINED = 'user-defined';
+    const TEMPLATE_TEXT = 'text';
     
     /**
      * @inheritdoc
