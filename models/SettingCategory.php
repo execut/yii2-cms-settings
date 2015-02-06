5cf6429bbc10424c9ed475b973de4ec79008c0cb
Fabio Maggio <fabio@infoweb.be>
2015-02-06 11:39:48 +0100

    Remove timestamp behavior from the category model

--- models/SettingCategory.php
+++ models/SettingCategory.php
@@ -3,9 +3,7 @@
 namespace infoweb\settings\models;
 
 use Yii;
-use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
-use yii\helpers\ArrayHelper;
 
 class SettingCategory extends \yii\db\ActiveRecord
 {
@@ -15,20 +13,6 @@ class SettingCategory extends \yii\db\ActiveRecord
     public static function tableName()
     {
         return 'settings_categories';
-    }
-
-    public function behaviors()
-    {
-        return ArrayHelper::merge(parent::behaviors(), [
-            'timestamp' => [
-                'class' => TimestampBehavior::className(),
-                'attributes' => [
-                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
-                    ActiveRecord::EVENT_BEFORE_UPDATE => 'updated_at',
-                ],
-                'value' => function() { return time(); },
-            ]
-        ]);
     }
 
     /**
